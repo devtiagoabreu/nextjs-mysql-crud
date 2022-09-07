91490692bebf01a97b090e0f58971389139796c6
Tiago de Abreu <devtiagoabreu@gmail.com>
2022-09-07 08:11:48 -0300

    inserindo um produto

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,7 +1,19 @@
+import axios from "axios";
+
 export function ProductForm() {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await axios.post('/api/products', {
+            name: 'product 1',
+            description: 'some product',
+            price: 1000
+        })
+        console.log(res)
+    }; 
+
   return (
-    <div>
-        <form>
+    <div className="bg-gray-300">
+        <form onSubmit={handleSubmit}>
             <label htmlFor='name'>Name:</label>
             <input type='text' name='name' id="name" />
 
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,9 +1,29 @@
-export default function handler(req, res) {
+import { pool } from "../../../config/db";
 
-    if (req.method === 'POST'){
-        return res.status(200).json('ceating products');
-    }else {
-        return res.status(200).json('Getting products');
+export default async function handler(req, res) {
+
+    switch(req.method) {
+        case 'GET': 
+            return res.status(200).json('Getting products');
+        case 'POST':
+            const {name, description, price} = req.body  
+          
+            const [result] = await pool.query('INSERT INTO product SET ?', {
+                name, 
+                description, 
+                price
+            });
+        
+            console.log(result)
+
+            return res
+                .status(200)
+                .json({
+                    name, 
+                    description, 
+                    price,
+                    id: result.insertId
+                });
     }
     
 }
