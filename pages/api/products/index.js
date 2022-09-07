70ba43784e2a6d30ee63d67bcf2e7481d2030ba8
Tiago de Abreu <devtiagoabreu@gmail.com>
2022-09-07 11:41:29 -0300

    iniciando metodo GET em products

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -3,27 +3,35 @@ import { pool } from "../../../config/db";
 export default async function handler(req, res) {
 
     switch(req.method) {
-        case 'GET': 
-            return res.status(200).json('Getting products');
+        case 'GET':
+            return await getAllProducts(req, res)
         case 'POST':
-            const {name, description, price} = req.body  
-          
-            const [result] = await pool.query('INSERT INTO product SET ?', {
-                name, 
-                description, 
-                price
-            });
-        
-            console.log(result)
-
-            return res
-                .status(200)
-                .json({
-                    name, 
-                    description, 
-                    price,
-                    id: result.insertId
-                });
+            return await saveProduct(req, res)
     }
-    
 }
+
+const getAllProducts = async (req, res) => {
+    const [result] = await pool.query('SELECT * FROM product'); 
+    console.log(result)
+    return res.status(200).json('Getting products');
+};
+
+const saveProduct = async (req, res) => {
+    const {name, description, price} = req.body  
+        
+    const [result] = await pool.query('INSERT INTO product SET ?', {
+        name, 
+        description, 
+        price,
+    });
+
+    return res
+        .status(200)
+        .json({
+            name, 
+            description, 
+            price,
+            id: result.insertId
+        });
+};
+    
